96df223a5b53c1824f7350ff401c3e0f77b78d12
William Eddy <eddyw@cardiff.ac.uk>
2022-11-10 21:20:37 +0000

    routing configuration complete and example in helloWorld page added

--- e2s-app/next.config.js
+++ e2s-app/next.config.js
@@ -1,39 +1,9 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n
 }
-
-module.exports = {
-  i18n: {
-    // These are all the locales you want to support in
-    // your application
-    locales: ['en','en-gb', 'cy'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: 'en-gb',
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
-    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
-    domains: [
-      {
-        domain: 'e2s-console.com',
-        defaultLocale: 'en-US',
-      },
-      {
-        domain: 'example.nl',
-        defaultLocale: 'nl-NL',
-      },
-      {
-        domain: 'example.fr',
-        defaultLocale: 'fr',
-        // an optional http field can also be used to test
-        // locale domains locally with http instead of https
-        http: true,
-      },
-    ],
-  },
-}
-
 
 module.exports = nextConfig
