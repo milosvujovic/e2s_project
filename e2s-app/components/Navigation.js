6aff2c69901ab21915b5467b83a749959e6b490c
c1989961 <InegbedionOI@cardiff.ac.uk>
2022-11-15 13:13:09 +0000

    added the components in the navbar and enabled the routing function to individual pages

--- e2s-app/components/Navigation.js
+++ e2s-app/components/Navigation.js
@@ -1,4 +1,5 @@
 import { createStyles } from '@mantine/core';
+import Link from 'next/Link';
 
 const useStyles = createStyles((theme, _params) => ({
     navigationParent:{
@@ -13,7 +14,59 @@ export function Navigation() {
 
     return (
         <>
-            <div className={classes.navigationParent}/>
+            {/*<h1>Logo</h1>*/}
+            {/*<div className={classes.navigationParent}/>*/}
+
+            <div  className={classes.navigationParent}>
+                <h1>
+                    Logo
+                </h1>
+                <div>
+                    <div>
+                        <div>
+                            <h3>
+                                <Link href="/dashboard">
+                                Dashboard
+                                </Link>
+                            </h3>
+                        </div>
+                        <div>
+                            <h3>
+                                <Link href="/usage">
+                                    Usage
+                                </Link>
+                            </h3>
+                        </div>
+                        <div>
+                            <h3>
+                                <Link href="/infrastructure">
+                                    Infrastructure
+                                </Link>
+                            </h3>
+                        </div>
+                        <div>
+                            <h3>
+                                <Link href="/reporting">
+                                    Reporting
+                                </Link>
+                            </h3>
+                        </div>
+                    </div>
+
+                    {/* setting  */}
+                    <div>
+                        <div>
+                            <h3>
+                                <Link href="/settings">
+                                    Settings
+                                </Link>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </>
     );
 }
