c570e63ac157339d4852db7cb0d430f91cbf3f80
William-Eddy <william-eddy@outlook.com>
2022-11-21 15:58:37 +0000

    resolving app configuration issue

--- e2s-app/pages/_app.tsx
+++ e2s-app/pages/_app.tsx
@@ -6,7 +6,7 @@ import useSWR, { SWRConfig } from 'swr';
 import { appWithTranslation } from 'next-i18next';
 
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
 
     return (
         <SWRConfig 
