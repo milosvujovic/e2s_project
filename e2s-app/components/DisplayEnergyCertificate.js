4f1e62aaafc8ae285a433d6df7d54a0f17f30b1c
Milos Vujovic <vujovicm@cardiff.ac.uk>
2022-11-30 18:55:53 +0000

    added display energy certificate and boxes to infrastructure page

---
+++ e2s-app/components/DisplayEnergyCertificate.js
@@ -0,0 +1,29 @@
+import { Image } from "@mantine/core"
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme, _params) => ({
+    tmpContainer:{
+        margin: "10px 10px 0 10px",
+        padding: "20px",
+        borderRadius: "10px",
+        boxShadow: "0px 0px 10px rgb(0 0 0 / 4%)",
+        background:"white",
+        width:"600px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+    }
+}))
+
+
+function DisplayEnergyCertificate() {
+    const { classes } = useStyles();
+    return (
+        <div className={classes.tmpContainer}>
+            <Image src = "displayEnergyCertificate.svg"
+            width={259}
+            height={301}/>
+        </div>)
+}
+
+export default DisplayEnergyCertificate
