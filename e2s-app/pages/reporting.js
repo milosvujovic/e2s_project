884a2723bf33e3b6b7cef598e210fb5cf4bac8f6
Milos Vujovic <vujovicm@cardiff.ac.uk>
2022-12-02 10:57:06 +0000

    added list within container with basic site visit tips

--- e2s-app/pages/reporting.js
+++ e2s-app/pages/reporting.js
@@ -1,11 +1,24 @@
 import { createStyles } from '@mantine/core';
+import { AlignLeft } from 'tabler-icons-react';
 import AppShellConsole from "../components/AppShell";
 import { getUser } from '../hooks/useAuth';
 
 const useStyles = createStyles((theme, _params) => ({
-	/* Page styling goes here */
-	button:{
-		border:"1px solid blue"
+	h3:{
+		margin:"0px 0px 10px 0px"
+	},
+	ul:{
+		style:"list-style-type:disc;"
+	},
+	tmpContainer:{
+		margin: "10px 10px 0 10px",
+		padding: "20px",
+		borderRadius: "10px",
+		boxShadow: "0px 0px 10px rgb(0 0 0 / 4%)",
+		background:"white",
+		display: "flex",
+		flexDirection: "column",
+		justifyContent:"center"
 	}
 }))
 
@@ -20,9 +33,15 @@ export default function Reporting({user}) {
 
   return (
 	  /* HTML page content goes between AppShellConsole tags */
-	  <AppShellConsole title={"Reporting"} user={user}>
-
-	  </AppShellConsole>
-  );
+	  	<AppShellConsole title={"Reporting"} user={user}>
+		  	<div className={classes.tmpContainer} style ={{width:300}}>
+				<h3 className={classes.h3}>Tips when visiting a site</h3>
+				<ul className={classes.ul}>
+  					<li>Bring personal protective equipment</li>
+  					<li>Bring tools</li>
+  					<li>Bring identification</li>
+				</ul>
+			</div>
+	  	</AppShellConsole>
+  	);
 }
-
