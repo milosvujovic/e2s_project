1708c22b655faf576a9aa1bf94e2fd1efd896dd8
c1989961 <InegbedionOI@cardiff.ac.uk>
2022-12-03 20:08:16 +0000

    added a modal feature(add goals) using matine

--- e2s-app/pages/dashboard.js
+++ e2s-app/pages/dashboard.js
@@ -1,6 +1,9 @@
-import { createStyles } from '@mantine/core';
+// import {createStyles, Group} from '@mantine/core';
 import AppShellConsole from "../components/AppShell";
 import { getUser } from '../hooks/useAuth';
+import { useState } from 'react';
+import {Group, Button, Text, createStyles, Modal} from '@mantine/core';
+import { openConfirmModal, closeAllModals } from '@mantine/modals';
 
 const useStyles = createStyles((theme, _params) => ({
 	/* Page styling goes here */
@@ -16,10 +19,27 @@ export async function getServerSideProps(context) {
 
 export default function Dashboard({user}) {
 	const { classes } = useStyles();
+	const [opened, setOpened] = useState(false);
+
   return (
 	  /* HTML page content goes between AppShellConsole tags */
 	  <AppShellConsole title={"Dashboard"} user={user}>
 			<h1>Hello</h1>
+
+		  <>
+			  <Modal
+				  opened={opened}
+				  onClose={() => setOpened(false)}
+				  title="Add Goals!"
+			  >
+				  {/* Modal content */}
+			  </Modal>
+
+			  <Group position="center">
+				  <Button onClick={() => setOpened(true)}>+</Button>
+			  </Group>
+		  </>
+
 	  </AppShellConsole>
   );
 }
