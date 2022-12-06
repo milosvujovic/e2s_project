62896a1179a0290d16cac5ae61bfa24def49aec1
c1989961 <InegbedionOI@cardiff.ac.uk>
2022-12-06 13:27:06 +0000

    finished form for kpi

--- e2s-app/pages/dashboard.js
+++ e2s-app/pages/dashboard.js
@@ -1,9 +1,13 @@
-// import {createStyles, Group} from '@mantine/core';
 import AppShellConsole from "../components/AppShell";
 import { getUser } from '../hooks/useAuth';
 import { useState } from 'react';
-import {Group, Button, Text, createStyles, Modal} from '@mantine/core';
+import {Group, Button, Text, createStyles, Modal, useMantineTheme} from '@mantine/core';
 import { openConfirmModal, closeAllModals } from '@mantine/modals';
+import { TextInput, Checkbox } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { Select, Textarea, Radio } from '@mantine/core';
+import { DatePicker } from '@mantine/dates';
+import kpi from "./api/kpi";
 
 const useStyles = createStyles((theme, _params) => ({
 	/* Page styling goes here */
@@ -20,8 +24,62 @@ export async function getServerSideProps(context) {
 export default function Dashboard({user}) {
 	const { classes } = useStyles();
 	const [opened, setOpened] = useState(false);
+	const theme = useMantineTheme();
+	const form = useForm( {
+		initialValues: {
+			name: '',
+			type: '',
+			description: '',
+			date: '',
+			target: '',
+			unit: '',
+			unitIsPrefix: '',
+		},
+		validate: {
+			name: (value) => (value.length < 2 ? 'Invalid' : null),
+			type: (value) => (value.length < 2 ? 'Invalid' : null),
+			date: (value) => (value.length < 2 ? 'Invalid' : null),
+			unit: (value) => (value.length < 2 ? 'Invalid' : null),
 
-  return (
+		},
+	});
+	const [data, setData] = useState([
+		{ value: 'react', label: 'Budget' },
+		{ value: 'c02_emission', label: 'C02 Emission' },
+	]);
+	const submitReading = async (event) =>{
+		event.preventDefault()
+
+		const data = JSON.stringify({
+			name: event.target[0].value,
+			type: event.target[2].value,
+			description: event.target[3].value,
+			date: event.target[4].value,
+			target: event.target[6].value,
+			unit: event.target[7].value,
+			unitIsPrefix: checked
+		})
+
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: data
+		}
+
+		const response = await fetch('/api/kpi', options)
+
+		event.target.reset(kpi());
+		alert("Goal added ");
+
+	}
+	// const [value, setValue] = useState('');
+	const [checked, setChecked] = useState(false);
+
+
+
+	return (
 	  /* HTML page content goes between AppShellConsole tags */
 	  <AppShellConsole title={"Dashboard"} user={user}>
 			<h1>Hello</h1>
@@ -30,12 +88,82 @@ export default function Dashboard({user}) {
 			  <Modal
 				  opened={opened}
 				  onClose={() => setOpened(false)}
-				  title="Add Goals!"
+				  title="Create Goal!"
+				  overlayColor={theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
+				  overlayOpacity={0.55}
+				  overlayBlur={3}
 			  >
-				  {/* Modal content */}
+					  <form className="kpi" action="/api/kpi" method="post" onSubmit={submitReading} onReset={form.onReset}>
+					  <TextInput
+						  withAsterisk
+						  label="KPI name"
+						  placeholder="Goal Name"
+						  {...form.getInputProps('name')}
+					  />
+					  <Select
+						  withAsterisk
+						  label="KPI type"
+						  placeholder="Goal Type"
+						  {...form.getInputProps('type')}
+						  data={data}
+						  nothingFound="Nothing found"
+						  searchable
+						  creatable
+						  getCreateLabel={(query) => `+ Create ${query}`}
+						  onCreate={(query) => {
+							  const item = { value: query, label: query };
+							  setData((current) => [...current, item]);
+							  return item;
+						  }}
+					  />
+					  <Textarea
+						  placeholder="Goal Description"
+						  label="KPI description"
+						  autosize
+						  minRows={2}
+						  {...form.getInputProps('description')}
+					  />
+					  <DatePicker
+						  placeholder="date"
+						  label="Reach By"
+						  withAsterisk
+						  {...form.getInputProps('date')}
+					  />
+					  <TextInput
+						  // withAsterisk
+						  label="Target Value "
+						  placeholder="target value"
+						  {...form.getInputProps('target')}
+					  />
+					  <TextInput
+						  withAsterisk
+						  label="Unit "
+						  placeholder="Unit"
+						  {...form.getInputProps('unit')}
+					  />
+					  {/*<Radio.Group*/}
+						{/*  value={value}*/}
+						{/*  onChange={setValue}*/}
+						{/*  {...form.getInputProps('unitIsPrefix')}*/}
+						{/*  name="favoriteFramework">*/}
+
+						{/*  <Radio value="true" label="prefix"/>*/}
+						{/*  <Radio value="false" label="suffix"/>*/}
+					  {/*</Radio.Group>*/}
+						  <br></br>
+					  <Checkbox
+						  label="Is Prefix"
+						  checked={checked}
+						  onChange={(event) => setChecked(event.currentTarget.checked)}
+					  />
+
+					  <Group position="right" mt="md">
+						  <Button type="submit">Submit</Button>
+					  </Group>
+				  </form>
 			  </Modal>
 
-			  <Group position="center">
+			  <Group position="left">
 				  <Button onClick={() => setOpened(true)}>+</Button>
 			  </Group>
 		  </>
