5b63b7bce6c6a5c0e7e631f1eb25be00607293bb
c1989961 <InegbedionOI@cardiff.ac.uk>
2022-12-12 16:14:40 +0000

    merged the meter reading with dev

--- e2s-app/pages/api/readings.js
+++ e2s-app/pages/api/readings.js
@@ -1,28 +1,30 @@
 import db from '../../db';
 
 export default async function (req, res) {
+    if (!req) {
+        return
+    }
+    const body = req.body
+    console.log('body: ', body)
 
-const body = req.body
 
-
-console.log('body: ', body)
-
-
-if (!body.type || !body.fig) {
-    return res.status(400).json({ data: 'Reading data not found' })
-}
-    db.putItem(
+// if (!body.type || !body.fig) {
+//     return res.status(400).json({ data: 'Reading data not found' })
+// }
+    db.put(
         {
             "TableName": "TimeSeriesData",
             "Item": {
-                "name": {"S": ""},
+                "companyName": "testcompany",
+                // "name": {"S": ""},
+                "name": body.type,
+                // "timestamp": {"N": ""},
                 "timestamp": {"N": ""},
-                "value": {"N": ""}
+                // "value": {"N": ""}
+                "value": body.value
             }
         }, function(result) {
-            result.on('data', function(chunk) {
-                console.log("" + chunk);
-            });
+            console.log(result);
         });
     console.log("Items are succesfully ingested in table ..................");
 
